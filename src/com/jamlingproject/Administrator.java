350721ebcc36537230427e220489e0abc2b0e3f1
Jamling <jamlingg@gmail.com>
2016-03-26 21:37:40 -0400

    Remote change

--- src/com/jamlingproject/Administrator.java
+++ src/com/jamlingproject/Administrator.java
@@ -34,7 +34,7 @@ public class Administrator{
 		patient.setPhone_number(in.next());
 		System.out.println("Please enter Insurance Carrier");
 		patient.setInsurance(in.next());
-		System.out.println("Please enter GOOGLE Carrier");
+		System.out.println("Please enter Amazon Carrier");
 		personList.add(patient);
 		hashMap.put(patient.getFirst_name(),personList);
 		
